70d6625331729e43a37a8bb2a90d6cd4d0b72668
Campos,Carmen (IT RDM) BI-ES-S <carmen.campos@boehringer-ingelheim.com>
2022-11-03 14:33:14 +0100

    predict my images

--- dog_app.py
+++ dog_app.py
@@ -810,11 +810,17 @@ predict_dog_breed('images')
 # 
 # __Answer:__ 
 
-# In[ ]:
+# In[96]:
 
 
 ## TODO: Execute your algorithm from Step 6 on
 ## at least 6 images on your computer.
 ## Feel free to use as many code cells as needed.
 predict_dog_breed('my_images')
+
+
+# In[ ]:
+
+
+
 
